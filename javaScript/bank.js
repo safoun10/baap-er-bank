aa928699449f7a23cece66c4e6fb286be283a5c3
Safoun <safoun.official@gmail.com>
2023-03-08 19:31:51 +0600

    fully validated input data

--- javaScript/bank.js
+++ javaScript/bank.js
@@ -1,49 +1,52 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
-    const depositData = document.getElementById("input-deposit-amount");
-    const updatingDepositData = parseFloat(depositData.value);
 
+    const depositData = document.getElementById("input-deposit-amount");
+    const totalDeposit = document.getElementById("total-deposit");
+    const balance = document.getElementById("current-balance");
+
+    const updatingDepositData = parseFloat(depositData.value);
+    const updatingTotalDeposit = parseFloat(totalDeposit.innerText);
+    const currentBalance = parseFloat(balance.innerText);
+    
     if (typeof updatingDepositData !== "number" || isNaN(updatingDepositData)) {
         alert("Please input a valid number!!");
         return;
     }
-
-    depositData.value = "";
-
-    const totalDeposit = document.getElementById("total-deposit");
-    const updatingTotalDeposit = parseFloat(totalDeposit.innerText);
-
+    
     const newDepositData = updatingTotalDeposit + updatingDepositData;
     totalDeposit.innerText = newDepositData;
-
-    const balance = document.getElementById("current-balance");
-    const currentBalance = parseFloat(balance.innerText);
-
+    
     const totalBalance = currentBalance + updatingDepositData;
     balance.innerText = totalBalance;
-
+    
+    depositData.value = "";
 })
 
 document.getElementById("withdraw-btn").addEventListener("click", function () {
-    const withdrawAmount = document.getElementById("input-withdraw-amount");
-    const updatedWithdrawAmount = parseFloat(withdrawAmount.value);
 
+    const withdrawAmount = document.getElementById("input-withdraw-amount");
+    const withdraw = document.getElementById("withdraw-money");
+    const balance = document.getElementById("current-balance");
+
+    const updatedWithdrawAmount = parseFloat(withdrawAmount.value);
+    const updatedWithdraw = parseFloat(withdraw.innerText);
+    const currentBalance = parseFloat(balance.innerText);
+    
     if (typeof updatedWithdrawAmount !== "number" || isNaN(updatedWithdrawAmount)) {
         alert("Please input a valid number!!");
         return;
     }
-
-    withdrawAmount.value = "";
-
-    const withdraw = document.getElementById("withdraw-money");
-    const updatedWithdraw = parseFloat(withdraw.innerText);
-
+    
+    if (updatedWithdrawAmount > currentBalance) {
+        alert("Sorry , you can't withdraw more than your balance!!");
+        return;
+    }
+    
     const totalWithdraw = updatedWithdrawAmount + updatedWithdraw;
-
     withdraw.innerText = totalWithdraw;
-
-    const balance = document.getElementById("current-balance");
-    const currentBalance = parseFloat(balance.innerText);
-
+    
     const totalBalance = currentBalance - updatedWithdrawAmount;
     balance.innerText = totalBalance;
+
+    withdrawAmount.value = "";
 })
